4fb8217877f765d16345f2b354372495775f2c2e
Stephen Dennis <brazilofmux@gmail.com>
2024-12-07 03:33:38 -0600

    Start cleaning up rstest.cpp.

--- rs/rstest/rstest.cpp
+++ rs/rstest/rstest.cpp
@@ -1,15 +1,44 @@
 // Copyright (C) 1996-1997 Solid Vertical Domains, Ltd. All rights reserved.
-//
-// Solid Vertical Domains, Ltd.
-// 11410 NE 124th ST #162, Kirkland WA 98034-4399 USA
-// (206) 488-0378
-// Stephen V. Dennis <SDennis@accessone.com>
+// Stephen V. Dennis <brazilofmux@gmail.com>
 //
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <cstdint>
 #include "rs.h"
+
+enum class Verbosity {
+    Quiet,      // Summary only
+    Normal,     // Basic progress
+    Debug       // Full details (old behavior)
+};
+
+struct RsTestConfig {
+    int tt;                     // Error correction capacity
+    int num_codewords;          // Default 10000
+    Verbosity verbose_level;
+    unsigned int random_seed;   // For reproducibility
+    double error_rate;          // Override default tt/(8*nn_short)
+    bool verify_correction;     // Extra validation
+};
+
+struct RsTestFileHeader {
+    uint32_t magic;            // Identifier
+    uint32_t version;          // Format version
+    uint32_t tt;               // Error correction capacity
+    uint32_t num_codewords;    // How many vectors follow
+    uint32_t codeword_size;    // Total size (parity + data)
+};
+
+struct DecodingStats {
+    int total_codewords;
+    int total_errors_injected;
+    int errors_corrected;
+    int decode_failures;
+    int errors_by_count[MAX_TT + 1];  // Distribution of error counts
+    int uncorrectable_errors;         // When errors > tt
+};
 
 #if 1
 long int nrand48(unsigned short seed[3])
@@ -52,32 +81,52 @@ int kk;
 GF data[MAX_KK];
 GF recd[nn];
 GF bb[2*MAX_TT];
+RS_ENCODER *prs;
 
 int mode = 0;
 #define ENCODING 1
 #define DECODING 2
 
-RS_ENCODER *prs;
+RsTestConfig parse_args(int argc, char* argv[]) {
+    RsTestConfig config = {
+        .tt = 0,
+        .num_codewords = 10000,
+        .verbose_level = Verbosity::Normal,
+        .random_seed = 1093,
+        .error_rate = 0.0,
+        .verify_correction = true
+    };
+
+    if (argc < 3) {
+        printf("Usage: rstest [-e|-d] tt [-n num_codewords] [-v level] [-s seed]\n");
+        exit(1);
+    }
+
+    // Parse basic mode and tt first
+    if (argv[1][0] == '-') {
+        if (argv[1][1] == 'e' || argv[1][1] == 'E') {
+            mode = ENCODING;
+        } else if (argv[1][1] == 'd' || argv[1][1] == 'D') {
+            mode = DECODING;
+        } else {
+            printf("Unknown mode %s\n", argv[1]);
+            exit(1);
+        }
+    }
+
+    config.tt = tt = atoi(argv[2]);  // Set both config and global tt
+    if (config.tt < MIN_TT || config.tt > MAX_TT) {
+        printf("tt must be between %d and %d\n", MIN_TT, MAX_TT);
+        exit(1);
+    }
+
+    return config;
+}
 
 int main(int argc, char *argv[])
 {
-    if (argc == 3 && argv[1][0] == '-' && (argv[1][1] == 'e' || argv[1][1] == 'E'))
-    {
-        mode = ENCODING;
-        tt = atoi(argv[2]);
-        kk = nn - 2*tt;
-    }
-    else if (argc == 3 && argv[1][0] == '-' && (argv[1][1] == 'd' || argv[1][1] == 'D'))
-    {
-        mode = DECODING;
-        tt = atoi(argv[2]);
-        kk = nn - 2*tt;
-    }
-    else
-    {
-        printf("Usage: rstest [-e|-d] tt\n");
-        exit(0);
-    }
+    RsTestConfig config = parse_args(argc, argv);
+    kk = nn - 2*tt;
 
     RS_Init();
 
@@ -151,12 +200,12 @@ int main(int argc, char *argv[])
 
         /* compute dimension of shortened code */
         kk_short = kk - (nn-nn_short);
-        no_cws = 1000000;
+        no_cws = 10000;
         for (i=0;i < 3;i++)
         {
             store[i] = 109-i*5; // store[i] = tmp;
         }
-      
+
 #ifndef PROFILE
     	if (!rs_fopen(&fp_out, cw_file, "wb") || nullptr == fp_out)
         {
@@ -164,7 +213,7 @@ int main(int argc, char *argv[])
             exit(0);
         }
 #endif
-  
+
         /**** BEGIN: Encoding random information vectors ****/
         /* Pad with zeros rightmost (kk-kk_short) positions */
         memset(data + kk_short, 0, kk - kk_short);
@@ -172,7 +221,7 @@ int main(int argc, char *argv[])
         {
             for ( j = 0; j < (int)kk_short; j++)
                 data[j] = (int) (nrand48(store) % 256);
-    
+
             prs->RSEncode(data, bb);
 
 #ifndef PROFILE
@@ -206,18 +255,18 @@ int main(int argc, char *argv[])
         seed = 1093; //scanf("%ld",&seed);
         srand(seed);
 
-        no_cws = 1000000; // printf("Enter no of codewords \n"); scanf("%d",&no_cws);
+        no_cws = 10000; // printf("Enter no of codewords \n"); scanf("%d",&no_cws);
 
-        if (rs_fopen(&fp, file_in, "rb") || nullptr == fp)
+        if (!rs_fopen(&fp, file_in, "rb") || nullptr == fp)
         {
             printf("Could not open %s\n", file_in);
             exit(0);
         }
-    
+
         prob_symb_err = (double)tt/ (8.0 * (double)nn_short);
         /* CHANGE above probablity of symbol error if desired */
         /* printf("Probability of symbol error = %6e\n",prob_symb_err);*/
-    
+
         no_decoder_errors = 0;
         iter = -1;
         while (++iter < no_cws)
@@ -233,7 +282,7 @@ int main(int argc, char *argv[])
             /* printf("The Transmitted Codeword\n");
             for (i=0;i < nn_short;i++) printf("%02x ",cw[i]); printf("\n");*/
 #endif
-        
+
 #ifndef NO_PRINT
             printf("\n\n\n\n Transmitting codeword %d \n",iter);
             printf("Channel caused following errors Location (Error): \n");
@@ -268,10 +317,10 @@ int main(int argc, char *argv[])
             for (i=0;i < nn-kk;i++)
                 recd[i] = received[i]; /* parity bytes */
             for (i=nn-kk;i < nn-kk+kk_short;i++)
-                recd[i] = received[i]; /* info bytes */ 
+                recd[i] = received[i]; /* info bytes */
             for (i=nn-kk+kk_short;i < nn;i++)
                 recd[i] = 0; /* zero padding */
-    
+
 #if 1
             decode_flag = prs->RSDecode(recd);
 #else
@@ -282,7 +331,7 @@ int main(int argc, char *argv[])
 #endif
             error_flag = 0;
             for (i=0; i < kk_short; i++)
-            { 
+            {
                 if (recd[i+nn-kk] != cw[i+nn-kk])
                 {
                     if (no_ch_errs <= tt)
@@ -294,7 +343,7 @@ int main(int argc, char *argv[])
                     //break;
                 }
             }
-    
+
             if (decode_flag == RS_ERROR_LAMDA_ERROR && no_ch_errs <= tt)
             {
                 printf("%d ch errs  <=  max # correctable errs but \n", no_ch_errs);
@@ -316,9 +365,9 @@ int main(int argc, char *argv[])
                 for (i = 0; i < nn_short; i++)
                     printf("%02x ", cw[i]);
                 printf("\n");
-#endif 
+#endif
             }
-    
+
         } /* closing iteration loop */
         if (no_decoder_errors > 0)
             printf("The number of decoder errors were %d\n",no_decoder_errors);
