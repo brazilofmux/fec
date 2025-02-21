cab7303460336090eda40788a2be95d284e89d41
Stephen Dennis <brazilofmux@gmail.com>
2025-02-21 11:49:18 -0700

    Add RSGenPoly and fix RSGenTable.

--- rs/rstest/rs_encoder_t1.cpp
+++ rs/rstest/rs_encoder_t1.cpp
@@ -1,11 +1,13 @@
-#include <memory.h>
+#include <memory>
 #include "projdefs.h"
 #include "rs_encoder_t1.h"
 
 RS_ENCODER_T1::RS_ENCODER_T1(int b0)
     : RS_ENCODER_BASE(1, b0)  // tt is always 1
     , ptable(new GF[TABLE_SIZE])
+    , gg(2 * tt_ + 1)  // Size based on tt_
 {
+    RSGenPoly();
     RSGenTable();
 }
 
@@ -13,48 +15,71 @@ RS_ENCODER_T1::~RS_ENCODER_T1() {
     delete[] ptable;
 }
 
-void RS_ENCODER_T1::RSGenTable() {
-    const GF* pow2poly = get_pow2poly();
-    const GF* poly2pow = get_poly2pow();
+void RS_ENCODER_T1::RSGenPoly() {
+    // Initially, g(x) = (X+@^b0)
+    gg[0] = Pow2Poly[b0_];
+    gg[1] = 1;
 
-    // Generate the specialized table for tt=1
-    // Each feedback value needs 2 entries
-    for (int ch = 0; ch < 256; ch++) {
-        int feedback = poly2pow[ch];
-        if (feedback != GF_INFINITY) {
-            // First coefficient
-            int iMod = mod_nn(feedback + b0_);
-            ptable[ch * 2] = pow2poly[iMod];
+    for (int i = 2; i <= 2 * tt_; i++) {
+        gg[i] = 1;
 
-            // Second coefficient
-            iMod = mod_nn(feedback + b0_ + 1);
-            ptable[ch * 2 + 1] = pow2poly[iMod];
+        // Multiply (gg[0]+gg[1]*x + ... +gg[i]x^i) by (@^(b0+i-1) + x)
+        for (int j = i - 1; j > 0; j--) {
+            if (gg[j] != 0) {
+                int iMod = mod_nn(Poly2Pow[gg[j]] + b0_ + i - 1);
+                gg[j] = gg[j - 1] ^ Pow2Poly[iMod];
+            }
+            else {
+                gg[j] = gg[j - 1];
+            }
         }
-        else {
-            ptable[ch * 2] = 0;
-            ptable[ch * 2 + 1] = 0;
+
+        // gg[0] can never be zero
+        int iMod = mod_nn(Poly2Pow[gg[0]] + b0_ + i - 1);
+        gg[0] = Pow2Poly[iMod];
+    }
+
+    // Convert gg[] to power form for quicker encoding
+    for (int i = 0; i <= 2 * tt_; i++) {
+        gg[i] = Poly2Pow[gg[i]];
+    }
+}
+
+void RS_ENCODER_T1::RSGenTable() {
+    // Generate table entries for each possible feedback value
+    for (int ch = 0; ch < 256; ch++) {
+        int feedback = Poly2Pow[ch];
+
+        // For tt=1, we generate two coefficients per feedback value
+        for (int j = 0; j < 2 * tt_; j++) {
+            if (gg[j + 1] != GF_INFINITY && feedback != GF_INFINITY) {
+                int iMod = mod_nn(gg[j + 1] + feedback);
+                ptable[ch * 2 * tt_ + j] = Pow2Poly[iMod];
+            }
+            else {
+                ptable[ch * 2 * tt_ + j] = 0;
+            }
         }
     }
 }
 
 void RS_ENCODER_T1::RSEncode(GF data[MAX_KK], GF bb[2 * MAX_TT]) {
-    // For tt=1, we can use a 16-bit LFSR implementation
-    // This is highly optimized for the tt=1 case
+    // Optimized encoder for tt=1 using 16-bit LFSR
     USHORT LFSR = 0;
     USHORT* pus = (USHORT*)data;
+    const int pairs = (kk_ - 1) / 2;  // Process pairs of bytes
 
-    // Process pairs of bytes using 16-bit operations
-    // For tt=1, we know exactly how many iterations
-    int cnt = 126;
-    while (cnt--) {
+    for (int cnt = 0; cnt < pairs; cnt++) {
         LFSR ^= *pus++;
         LFSR ^= ptable[2 * (GF)(LFSR)] << 8;
         LFSR ^= ptable[2 * (GF)(LFSR >> 8)];
     }
     *(USHORT*)bb = LFSR;
 
-    // Handle the final bytes
-    GF feedback = bb[0] ^ data[252];
-    bb[0] = bb[1] ^ ptable[2 * feedback];
-    bb[1] = feedback;
+    // Handle final byte if kk_ is odd
+    if (kk_ % 2) {
+        GF feedback = bb[0] ^ data[kk_ - 1];
+        bb[0] = bb[1] ^ ptable[2 * feedback];
+        bb[1] = feedback;
+    }
 }
--- rs/rstest/rs_encoder_t1.h
+++ rs/rstest/rs_encoder_t1.h
@@ -1,6 +1,7 @@
 #ifndef RS_ENCODER_T1_H
 #define RS_ENCODER_T1_H
 
+#include <vector>
 #include "rs_encoder_base.h"
 
 class RS_ENCODER_T1 final : public RS_ENCODER_BASE {
@@ -11,10 +12,13 @@ public:
     void RSEncode(GF data[MAX_KK], GF bb[2 * MAX_TT]) override;
 
 private:
+    void RSGenPoly();
     void RSGenTable();
 
-    static constexpr int TABLE_SIZE = 512;  // 256 * 2 for tt=1
+    // For tt=1: TABLE_SIZE = 256 * 2
+    static constexpr int TABLE_SIZE = 256 * 2;
     GF* ptable;
+    std::vector<GF> gg;
 };
 
 #endif
