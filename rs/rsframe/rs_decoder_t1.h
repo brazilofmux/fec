8d58d5d5ba42a5301a013715e3a4b70f985951a6
Stephen Dennis <brazilofmux@gmail.com>
2025-02-24 16:53:18 -0700

    Pick up new files from rstest.

--- rs/rsframe/rs_decoder_t1.h
+++ rs/rsframe/rs_decoder_t1.h
@@ -1,13 +1,19 @@
 #ifndef RS_DECODER_T1_H
 #define RS_DECODER_T1_H
 
+#include <vector>
 #include "rs_decoder_base.h"
 
 class RS_DECODER_T1 : public RS_DECODER_BASE {
 public:
-    RS_DECODER_T1(int tt, int b0) : RS_DECODER_BASE(tt, b0) {}
-    int RSDecode(GF recd[nn]) override { return -1; }
-    int RSDecodeErasures(GF recd[nn], int eras_pos[2 * MAX_TT], int no_eras) override { return -1; }
+    explicit RS_DECODER_T1(int b0);
+    ~RS_DECODER_T1() = default;
+
+    int RSDecode(GF recd[nn]) override;
+    int RSDecodeErasures(GF recd[nn], int eras_pos[2 * MAX_TT], int no_eras) override;
+
+private:
+    void calculate_syndromes(const GF recd[nn], std::vector<GF>& syndromes) override;
 };
 
-#endif
+#endif // RS_DECODER_T1_H
