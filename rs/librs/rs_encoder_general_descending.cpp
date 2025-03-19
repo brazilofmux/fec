4d73f2e47d5582c60d15889e49828caba22509ad
Stephen Dennis <brazilofmux@gmail.com>
2025-03-19 13:44:31 -0600

    Take unrolled loop optimization.

--- rs/librs/rs_encoder_general_descending.cpp
+++ rs/librs/rs_encoder_general_descending.cpp
@@ -62,15 +62,159 @@ void RS_ENCODER_GENERAL_DESCENDING::RSGenTable() {
     }
 }
 
+#if 0
 void RS_ENCODER_GENERAL_DESCENDING::RSEncode(GF data[MAX_KK], GF bb[2 * MAX_TT]) {
     const int NON_SYM_LEN = 2 * tt_ + 1;
     memset(bb, 0, 2 * tt_ * sizeof(GF));
+
     for (int i = kk_ - 1; i >= 0; i--) {
         const GF feedback = bb[2 * tt_ - 1] ^ data[i];
         GF* TableRow = ptable + NON_SYM_LEN * feedback;
-        for (int j = 2 * tt_ - 1; j > 0; j--) {
-            process_byte1(bb, TableRow, j);
+
+        // Process bytes in blocks where possible
+        int j = 2 * tt_ - 1;
+
+        // Process 8-byte blocks
+        while (j >= 8) {
+            process_block8(&bb[j-7], &TableRow[j-7]);
+            j -= 8;
         }
+
+        // Process 4-byte blocks
+        if (j >= 4) {
+            process_block4(&bb[j-3], &TableRow[j-3]);
+            j -= 4;
+        }
+
+        // Process remaining bytes individually
+        while (j > 0) {
+            process_byte1(bb, TableRow, j);
+            j--;
+        }
+
         bb[0] = TableRow[0];
     }
 }
+#elif 0
+void RS_ENCODER_GENERAL_DESCENDING::RSEncode(GF data[MAX_KK], GF bb[2 * MAX_TT]) {
+    const int NON_SYM_LEN = 2 * tt_ + 1;
+    memset(bb, 0, 2 * tt_ * sizeof(GF));
+    
+    for (int i = kk_ - 1; i >= 0; i--) {
+        const GF feedback = bb[2 * tt_ - 1] ^ data[i];
+        GF* TableRow = ptable + NON_SYM_LEN * feedback;
+        
+        // Specialized code paths based on the common Reed-Solomon code sizes
+        switch (2 * tt_) {
+            case 32: // t=16, common for many Reed-Solomon implementations
+                process_block8(&bb[24], &TableRow[24]);
+                process_block8(&bb[16], &TableRow[16]);
+                process_block8(&bb[8], &TableRow[8]);
+                process_block8(&bb[0], &TableRow[0]);
+                break;
+                
+            case 16: // t=8, another common size
+                process_block8(&bb[8], &TableRow[8]);
+                process_block8(&bb[0], &TableRow[0]);
+                break;
+                
+            case 10: // t=5, used in some applications
+                process_block8(&bb[2], &TableRow[2]);
+                process_byte1(bb, TableRow, 1);
+                bb[0] = TableRow[0];
+                break;
+                
+            default: // General case with dynamic unrolling
+                int j = 2 * tt_ - 1;
+                
+                // Process 8-byte blocks
+                while (j >= 8) {
+                    process_block8(&bb[j-7], &TableRow[j-7]);
+                    j -= 8;
+                }
+                
+                // Process 4-byte blocks
+                if (j >= 4) {
+                    process_block4(&bb[j-3], &TableRow[j-3]);
+                    j -= 4;
+                }
+                
+                // Process remaining bytes individually
+                while (j > 0) {
+                    process_byte1(bb, TableRow, j);
+                    j--;
+                }
+                
+                bb[0] = TableRow[0];
+                break;
+        }
+    }
+}
+#elif 1
+void RS_ENCODER_GENERAL_DESCENDING::RSEncode(GF data[MAX_KK], GF bb[2 * MAX_TT]) {
+    const int NON_SYM_LEN = 2 * tt_ + 1;
+    const int parity_bytes = 2 * tt_;
+    
+    // Initialize parity bytes to zero
+    memset(bb, 0, parity_bytes * sizeof(GF));
+    
+    for (int i = kk_ - 1; i >= 0; i--) {
+        // Calculate feedback
+        const GF feedback = bb[parity_bytes - 1] ^ data[i];
+        
+        // Fast path for zero feedback (common case)
+        if (feedback == 0) {
+            // Just shift the registers
+            for (int j = parity_bytes - 1; j > 0; j--) {
+                bb[j] = bb[j - 1];
+            }
+            bb[0] = 0;
+            continue;
+        }
+        
+        GF* TableRow = ptable + NON_SYM_LEN * feedback;
+        
+        // Use optimized processing based on parity size and alignment
+        // We need to handle alignment properly for the block operations
+        
+        if ((parity_bytes % 8) == 0 && parity_bytes >= 8) {
+            // Perfectly aligned for 8-byte blocks
+            for (int j = parity_bytes - 8; j >= 0; j -= 8) {
+                process_block8(&bb[j], &TableRow[j]);
+            }
+        } 
+        else if ((parity_bytes % 4) == 0 && parity_bytes >= 4) {
+            // Perfectly aligned for 4-byte blocks
+            for (int j = parity_bytes - 4; j >= 0; j -= 4) {
+                process_block4(&bb[j], &TableRow[j]);
+            }
+        }
+        else {
+            // Mixed strategy for non-aligned sizes
+            int j = parity_bytes - 1;
+            
+            // Process 8-byte blocks where possible
+            while (j >= 7) {
+                process_block8(&bb[j-7], &TableRow[j-7]);
+                j -= 8;
+            }
+            
+            // Process 4-byte blocks where possible
+            if (j >= 3) {
+                process_block4(&bb[j-3], &TableRow[j-3]);
+                j -= 4;
+            }
+            
+            // Process remaining bytes individually
+            while (j >= 0) {
+                if (j > 0) {
+                    process_byte1(bb, TableRow, j);
+                } else {
+                    bb[0] = TableRow[0];
+                }
+                j--;
+            }
+        }
+    }
+}
+#endif
